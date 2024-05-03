ddc7e1d516e9a6239e6bd0248fb64860ed6356ec
pablodKampmann <pablo7kamp@gmail.com>
2024-05-03 12:33:52 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 'use client'
 
-import { Inconsolata } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/navBar";
-import LanguageOptions from "./components/languageOptions";
-import React, { useState } from 'react';
-import ToneMode from "./components/toneMode";
 
-const inter = Inconsolata({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -15,24 +11,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const [language, setLanguage] = useState('eng');
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="h-screen overflow-y-hidden relative bg-blue-950 bg-opacity-20">
-          <div className="absolute left-9 bottom-[18%] rounded-full bg-emerald-600 h-40 w-1 "></div>
-          <div className="absolute left-9 bottom-[46%] rounded-full bg-emerald-600 h-4 w-1 "></div>
-          <div className="absolute left-9 bottom-[54%] rounded-full bg-emerald-600 h-16 w-1 "></div>
-          <div className="absolute right-9 bottom-[50%] rounded-full bg-emerald-600 h-16 w-1 "></div>
-          <div className="absolute -right-5 bottom-[68%] font-semibold text-emerald-400 transform rotate-90">@pablokampmann</div>
-          <div className="absolute right-9 bottom-[80%] rounded-full bg-emerald-600 h-8 w-1 "></div>
-
-          <ToneMode/>
-          <LanguageOptions language={language} setLanguage={setLanguage} />
-          <NavBar language={language} />
-          {children}
-        </div>
+        {children}
       </body>
     </html>
   );
