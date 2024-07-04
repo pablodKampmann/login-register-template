f8a4b85b8906b7f1d38f7c053b00ca9a09966caa
pablodKampmann <pablo7kamp@gmail.com>
2024-07-04 12:07:27 -0300

    menu-update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
 
 export default function Home() {
 
   return (
-      
-      <div className="flex flex-col justify-center items-center">
-       
+    <div className="flex bg-white justify-center w-screen h-screen items-center">
+      <div className="w-fit px-10 h-fit py-16 shadow-xl rounded-lg bg-[#8EBEBD] text-3xl font-bold flex flex-col justify-center items-center">
+        Pick an option
+        <div className="space-x-4 mt-10 text-base tracking-tighter text-black">
+          <Link prefetch={true} href="/login" className="bg-white p-4 rounded-lg shadow-lg hover:bg-opacity-80 transition duration-150">LOGIN</Link>
+          <Link prefetch={true} href="/register" className="bg-white p-4 rounded-lg shadow-lg hover:bg-opacity-80 transition duration-150">REGISTER</Link>
+        </div>
       </div>
+    </div>
   );
 }
